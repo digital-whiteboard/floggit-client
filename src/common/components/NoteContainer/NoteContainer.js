ef6617e2fd905bed946844583cc8a0e6ba374c1b
den4is <d.piric@hotmail.com>
2017-09-29 16:50:22 +0200

    Added NoteContainer.js & NoteContainer.css (#6)
    * Added NoteContainer.js & NoteContainer.css
    
    * #2.1 edited render function in NoteContainer.js
    
    * Added NoteContainer.js & .CSS.  fixed lint in NoteList.js & CSS

--- src/common/components/NoteContainer/NoteContainer.js
+++ src/common/components/NoteContainer/NoteContainer.js
@@ -1,1 +1,57 @@
-// Our main JS file, takes care of our components.
+import React from 'react';
+
+import InputNotes from './components/InputNotes';
+import NoteList from './components/NoteList';
+import { getAll, add, remove } from '../../../utils/notes';
+import './NoteContainer.css';
+
+class NoteContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: [],
+    };
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  componentDidMount() {
+    getAll().then((notes) => {
+      this.setState({ notes });
+    });
+  }
+
+
+  handleAdd(value) {
+    add(value).then((id) => {
+      this.setState({
+        notes: this.state.notes.concat([{
+          id,
+          value,
+        }]),
+      });
+    });
+  }
+
+  handleRemove(id) {
+    remove(id).then(() => {
+      this.setState({
+        notes: this.state.notes.filter(noteContent => noteContent.id !== id),
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="NoteContainer-wrapper">
+        <InputNotes onAdd={this.handleAdd} />
+        <NoteList
+          notes={this.state.notes}
+          onNoteRemove={this.handleRemove}
+        />
+      </div>
+    );
+  }
+}
+
+export default NoteContainer;
