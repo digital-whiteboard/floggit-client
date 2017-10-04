6a8f2e96a79fdff01d0bd302db80ef74779c5b02
den4is <d.piric@hotmail.com>
2017-10-04 13:38:35 +0200

    #9 Installed Redux, react redux, redux thunk, created reduxStore

---
+++ src/reduxStore/config/notes.js
@@ -0,0 +1,101 @@
+import noteAPI from '../../utils/noteAPI';
+
+// ACTIONS
+const NOTE_ADD = 'NOTE_ADD';
+const NOTE_REMOVE = 'NOTE_REMOVE';
+const NOTE_LIST_REPLACE = 'NOTE_LIST_REPLACE';
+const NOTE_LOADING = 'NOTE_LOADING';
+const NOTE_LOADED = 'NOTE_LOADED';
+
+const initialState = {
+  data: [],
+  isLoading: false,
+  errorInfo: null,
+};
+// REDUCER
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case NOTE_ADD: {
+      const newNotes = [...state.data, action.data];
+      return Object.assign({}, state, { data: newNotes });
+    }
+    case NOTE_REMOVE: {
+      const newNotes = state.data.filter(note => note.id !== action.data.id);
+      return Object.assign({}, state, { data: newNotes });
+    }
+    case NOTE_LIST_REPLACE: {
+      const newNotes = [...action.data.notes];
+      return Object.assign({}, state, { data: newNotes });
+    }
+    case NOTE_LOADING: {
+      return Object.assign({}, state, { isLoading: true });
+    }
+    case NOTE_LOADED: {
+      return Object.assign({}, state, { isLoading: false });
+    }
+    default:
+      return state;
+  }
+};
+
+// ACTION CREATORS
+const internalAddNote = (id, value) => ({
+  type: NOTE_ADD,
+  data: {
+    id,
+    value,
+  },
+});
+
+const internalRemoveNote = id => ({
+  type: NOTE_REMOVE,
+  data: { id },
+});
+
+const internalReplaceAllNotes = notes => ({
+  type: NOTE_LIST_REPLACE,
+  data: {
+    notes,
+  },
+});
+
+const internalLoadingNotes = () => ({
+  type: NOTE_LOADING,
+});
+
+const internalLoadedNotes = () => ({
+  type: NOTE_LOADED,
+});
+
+// Redux thunk action creator
+// This is needed to work with async api's
+// for example your webservice.
+// A redux thunk action creator is a double arrow function.
+// The second part of the function is executed by redux thunk middleware
+// It is given the stores dispatcher and it can also recieve
+// the getState function if you need to know the current store state.
+
+const addNote = value => dispatch => noteAPI.add(value)
+  .then((id) => {
+    dispatch(internalAddNote(id, value));
+  });
+
+const removeNote = id => dispatch => noteAPI.remove(id)
+  .then(() => {
+    dispatch(internalRemoveNote(id));
+  });
+
+const loadNotes = () => (dispatch) => {
+  dispatch(internalLoadingNotes());
+  return noteAPI.getAll()
+    .then((notes) => {
+      dispatch(internalReplaceAllNotes(notes));
+      dispatch(internalLoadedNotes());
+    })
+    .catch(() => {
+      dispatch(internalLoadedNotes());
+    });
+};
+
+export { addNote, removeNote, loadNotes };
+export default reducer;
