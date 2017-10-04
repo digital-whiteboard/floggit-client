c651e0c6f0d9e4a252bba8d52e4c3705083727ca
den4is <d.piric@hotmail.com>
2017-10-04 13:49:14 +0200

    #9 Installed Redux, react redux, redux thunk, created reduxStore (#16)
    * #9 Installed Redux, react redux, redux thunk, created reduxStore
    
    * deleted comments

---
+++ src/reduxStore/index.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './config';
+import { loadNotes } from './config/notes';
+
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+store.dispatch(loadNotes());
+
+export default store;
